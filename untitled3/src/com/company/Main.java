059a852ee1af5c5fea7612bc7abfb5ede9409d2d
asadulla2007 <110012942+asadulla2007@users.noreply.github.com>
2022-07-30 02:27:51 -0400

    Add files via upload

---
+++ untitled3/src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.Scanner;
+
+import static java.lang.System.in;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        Scanner s = new Scanner(in);
+        String s1 = s.nextLine();
+        if (s1.length() >= 6) {
+            System.out.println(s1.concat("6"));
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
